f1824c6b134501b9bede1a9457658b2f32f3feb0
Conor <>
2022-01-14 11:39:58 -0700

    Feat: decrypted runner works as expected

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,4 +1,3 @@
-require "./lib/message_generator"
 require './lib/enigma'
 require 'date'
 require 'pry'
@@ -7,7 +6,7 @@ enigma = Enigma.new
 message_file = ARGV[0]
 encrypted_file = ARGV[1]
 message = File.read(message_file).chomp
-#binding.pry
+
 encrypted_message = enigma.encrypt(message)
-File.open('encrypted.txt', "w") {|f| f.write "#{encrypted_message[:decryption]}"}
+File.open('encrypted.txt', "w") {|f| f.write "#{encrypted_message[:encryption]}"}
 puts "Created #{encrypted_file} with the key #{encrypted_message[:key]} and date #{encrypted_message[:date]}"
